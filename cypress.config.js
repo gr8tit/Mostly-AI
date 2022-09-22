703d198cc09ef1591a4a5550436d75f7255c2230
Temitope Oyedemi <temitopeoyedemi@Temitopes-MacBook-Pro.local>
2022-09-22 05:06:43 +0100

    addded changes

--- cypress.config.js
+++ cypress.config.js
@@ -1,23 +1,8 @@
-const { defineConfig } = require("cypress");
-
-module.exports = defineConfig({
-  defaultCommandTimeout: 80000,
-  viewportWidth: 1000,
-  viewportHeight: 660,
-  projectId: "93jsp2",
-  // Viewport settings overridden for component tests
-  component: {
-    viewportWidth: 500,
-    viewportHeight: 500
-  },
-  reporter: 'junit',
-  reporterOptions: {
-  mochaFile: 'results/my-test-output-[hash].xml',
-  },
+module.exports = {
+  projectId: '93jsp2',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      baseUrl: 'https://mostly.ai/'
     },
   },
-});
+};
