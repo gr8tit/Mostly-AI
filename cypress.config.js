a01501bfb69c7eaf9e386c34880591ab3b9408ce
temitopeoyedemi <temmygr8t@gmail.com>
2022-09-24 03:08:27 +0100

    update workflow

--- cypress.config.js
+++ cypress.config.js
@@ -20,7 +20,7 @@ module.exports = defineConfig({
   },
 
   e2e: {
-    supportFile: false,
+    supportFile: support/this.e2e.js,
     specPattern: 'e2e/**/*.cy.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
       // implement node event listeners here
